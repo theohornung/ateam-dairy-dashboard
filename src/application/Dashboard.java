fdff4346b2d4b3cbcb50a075cb671be54e1fb7c4
tthornung@gmail.com <tthornung@gmail.com>
2020-04-28 17:06:59 -0500

    rm unused import in Dashboard

--- src/application/Dashboard.java
+++ src/application/Dashboard.java
@@ -7,7 +7,6 @@ import application.interfaces.IMilkList;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
